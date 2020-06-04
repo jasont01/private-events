e4143bb9007839955c6e4b49166251e39897c3a0
Jason Thompson <jason@jasont.us>
2020-06-04 19:57:21 -0400

    add devise auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # TODO change root to events index
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root to: 'users#index'
   resources :users
 end
