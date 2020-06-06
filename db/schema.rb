8af983559a5f59daa9a8ab31cadedc3730b7db4d
Jason Thompson <jason@jasont.us>
2020-06-05 23:13:11 -0400

    add some style

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_222551) do
+ActiveRecord::Schema.define(version: 2020_06_06_000533) do
+
+  create_table "events", force: :cascade do |t|
+    t.integer "creator_id"
+    t.string "event_name"
+    t.string "location"
+    t.datetime "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
